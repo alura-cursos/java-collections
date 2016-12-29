044487d74515829a3be305928d4359f236839575
Rodrigo Turini <rdrg.turini@gmail.com>
2016-12-29 14:37:38 -0200

    stage do inicio da aula 4

--- gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -1,23 +1,20 @@
 package br.com.alura;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TestaCurso {
 
 	public static void main(String[] args) {
+
 		Curso javaColecoes = new Curso("Dominando as coleções do Java",
-					"Paulo Silveira");
-		
+				"Paulo Silveira");
+
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
 		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
-		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
-				
-		List<Aula> aulasImutavel = javaColecoes.getAulas();
-		List<Aula> aulas = new ArrayList<>(aulasImutavel);
-		
-		System.out.println(aulas);
-		
-		
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+
+		System.out.println(javaColecoes.getAulas());
+
 	}
+
 }
--- gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
+++ gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
@@ -6,16 +6,16 @@ import java.util.Collections;
 public class TestandoListas {
 
 	public static void main(String[] args) {
+
 		String aula1 = "Conhecendo mais de listas";
 		String aula2 = "Modelando a classe Aula";
 		String aula3 = "Trabalhando com Cursos e Sets";
 
 		ArrayList<String> aulas = new ArrayList<>();
+
 		aulas.add(aula1);
 		aulas.add(aula2);
 		aulas.add(aula3);
-		
-		
 
 		System.out.println(aulas);
 
@@ -31,25 +31,23 @@ public class TestandoListas {
 		System.out.println("A primeira aula é " + primeiraAula);
 
 		for (int i = 0; i < aulas.size(); i++) {
-			System.out.println("aula : " + aulas.get(i));
+			System.out.println("Aula: " + aulas.get(i));
 		}
 
 		System.out.println(aulas.size());
 
 		aulas.forEach(aula -> {
-			System.out.println("percorrendo:");
+			System.out.println("Percorrendo: ");
 			System.out.println("Aula " + aula);
 		});
-		
+
 		aulas.add("Aumentando nosso conhecimento");
 		System.out.println(aulas);
-		
+
 		Collections.sort(aulas);
-		System.out.println( "Depois de ordenado:");
+
+		System.out.println("Depois de ordenado:");
 		System.out.println(aulas);
-		
-		
-		
-		
+
 	}
 }
