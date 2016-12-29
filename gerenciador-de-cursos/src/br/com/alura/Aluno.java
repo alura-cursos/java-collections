032ce1292431eb6d9efb3e96591371857aa571a8
Rodrigo Turini <rdrg.turini@gmail.com>
2016-12-29 14:46:05 -0200

    stage do inicio da aula 7

--- gerenciador-de-cursos/src/br/com/alura/Aluno.java
+++ gerenciador-de-cursos/src/br/com/alura/Aluno.java
@@ -6,9 +6,6 @@ public class Aluno {
 	private int numeroMatricula;
 
 	public Aluno(String nome, int numeroMatricula) {
-		if(nome == null) {
-			throw new NullPointerException("nome nao pode ser null");
-		}
 		this.nome = nome;
 		this.numeroMatricula = numeroMatricula;
 	}
@@ -20,20 +17,10 @@ public class Aluno {
 	public int getNumeroMatricula() {
 		return numeroMatricula;
 	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		Aluno outro = (Aluno) obj;		
-		return this.nome.equals(outro.nome);
-	}
-	
-	@Override
-	public int hashCode() {
-		return this.nome.hashCode();
-	}
 
 	@Override
 	public String toString() {
-		return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula + "]";
+		return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula
+				+ "]";
 	}
 }
--- gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,18 +1,16 @@
 package br.com.alura;
 
-import java.util.Iterator;
-import java.util.Set;
-import java.util.Vector;
-
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
-		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
+		
+		Curso javaColecoes = new Curso("Dominando as coleções do Java",
+				"Paulo Silveira");
 
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
 		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
 		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
-
+		
 		Aluno a1 = new Aluno("Rodrigo Turini", 34672);
 		Aluno a2 = new Aluno("Guilherme Silveira", 5617);
 		Aluno a3 = new Aluno("Mauricio Aniche", 17645);
@@ -22,42 +20,9 @@ public class TestaCursoComAluno {
 		javaColecoes.matricula(a3);
 		
 		System.out.println("Todos os alunos matriculados: ");
-
-		Set<Aluno> alunos = javaColecoes.getAlunos();
-		Iterator<Aluno> iterador = alunos.iterator();
-		while(iterador.hasNext()) {
-			Aluno proximo = iterador.next();
-			System.out.println(proximo);
-		}
-		
-		Vector<Aluno> vetor = new Vector<>();
-//		
-//		for(Aluno a : javaColecoes.getAlunos()) {
-//			System.out.println(a);
-//		}
-		
-		
-		
-		System.out.println("O aluno " + a1 + " está matriculado?");
-		System.out.println(javaColecoes.estaMatriculado(a1));
-		
-		
-		
-		
-		Aluno turini = new Aluno("Rodrigo Turini", 34672);
-		System.out.println("E esse Turini, está matriculado?");
-		System.out.println(javaColecoes.estaMatriculado(turini));
-		
-		
-		System.out.println("O a1 é equals ao Turini?");
-		System.out.println(a1.equals(turini));
-		
-		// obrigatoriamente o seguinte é true:
-		
-		System.out.println(a1.hashCode() == turini.hashCode());
-		
-		
-		
+		javaColecoes.getAlunos().forEach(aluno -> {
+			System.out.println(aluno);
+		});
 		
 	}
 }
