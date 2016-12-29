f07f5b52f8791b6591fb7bb9254a6f4a181dfa80
Rodrigo Turini <rdrg.turini@gmail.com>
2016-12-29 14:46:59 -0200

    stage do inicio da aula 8

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -1,14 +1,9 @@
 package br.com.alura;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class Curso {
@@ -17,7 +12,6 @@ public class Curso {
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
 	private Set<Aluno> alunos = new HashSet<>();
-	private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -36,6 +30,10 @@ public class Curso {
 		return Collections.unmodifiableList(aulas);
 	}
 
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 	}
@@ -46,28 +44,16 @@ public class Curso {
 
 	@Override
 	public String toString() {
-		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + "," + "aulas: " + this.aulas + "]";
+		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal()
+				+ ", aulas: + " + this.aulas + "]";
 	}
 
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
-		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
-	}
-	
-	
-	public Set<Aluno> getAlunos() {
-		return Collections.unmodifiableSet(alunos);
 	}
 
 	public boolean estaMatriculado(Aluno aluno) {
 		return this.alunos.contains(aluno);
 	}
 
-	public Aluno buscaMatriculado(int numero) {
-		if(!matriculaParaAluno.containsKey(numero))
-			throw new NoSuchElementException();
-		return matriculaParaAluno.get(numero);
-	}
-	
-	
 }
--- gerenciador-de-cursos/src/br/com/alura/TestaAlunos.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaAlunos.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class TestaAlunos {
 
 	public static void main(String[] args) {
+
 		Collection<String> alunos = new HashSet<>();
 		alunos.add("Rodrigo Turini");
 		alunos.add("Alberto Souza");
@@ -16,31 +17,18 @@ public class TestaAlunos {
 		alunos.add("Sergio Lopes");
 		alunos.add("Renan Saggio");
 		alunos.add("Mauricio Aniche");
-		alunos.add("Alberto Souza");
-		
-		
-		boolean pauloEstaMatriculado = alunos.contains("Paulo Silveira");
-		alunos.remove("Sergio Lopes");
-		
-		
-		System.out.println(pauloEstaMatriculado);
-		
-		System.out.println(alunos.size());
-		
-		for (String aluno : alunos) {
-			System.out.println(aluno);
-		}
-		
+
 		alunos.forEach(aluno -> {
 			System.out.println(aluno);
 		});
-		
+
 		System.out.println(alunos);
-		
-		
-		
-		
+
 		List<String> alunosEmLista = new ArrayList<>(alunos);
-		 
+		
+		Collections.sort(alunosEmLista);
+		
+		System.out.println(alunosEmLista);
 	}
+
 }
